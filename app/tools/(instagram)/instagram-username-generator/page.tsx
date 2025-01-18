eb5bdb0fd1a5d6e0ceb87622e5e4ab7a5bd65c88
ansariazaz <ansariazaz1997@gmail.com>
2025-01-18 15:46:19 +0530

    fix usename

--- app/tools/(instagram)/instagram-username-generator/page.tsx
+++ app/tools/(instagram)/instagram-username-generator/page.tsx
@@ -1,7 +1,19 @@
 "use client";
 import Loader from "@/components/Loader";
 import MobileView from "@/components/MobileView";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import Image from "next/image";
+import checklist from "@/assets/icons/checklist.png";
+import preview from "@/assets/icons/preview.svg";
+import { useToast } from "@/hooks/use-toast";
+import copy from "@/assets/icons/copy.svg";
 import { useState } from "react";
+import { Card } from "@/components/ui/card";
 
 interface CategoryData {
   [key: string]: string[];
@@ -212,7 +224,7 @@ const page = () => {
   const [loading, setLoading] = useState<boolean>(false);
   const [usernames, setUsernames] = useState<string[]>([]);
   const [username, setUsername] = useState<string>("");
-
+  const { toast } = useToast()
   const handleCategoryChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     setCategory(e.target.value);
   };
@@ -229,10 +241,31 @@ const page = () => {
       const results: string[] = [];
       console.log(keywords, "keywords");
       keywords.forEach((term) => {
-        results.push(`${keyword}${term}`);
-        results.push(`${term}${keyword}`);
+        results.push(
+          `${keyword}${term}`,
+          `${term}_`,
+          `${term}${Math.floor(Math.random() * 100)}`,
+          `the_${term}`,
+          `${term}_official`,
+          `real_${term}`,
+          `_${term}`,
+          `${term}_${Math.floor(Math.random() * 1000)}`,
+          `${keyword}_${term}`,
+          `${term}_${keyword}`,
+          `${keyword}The${term}`,
+          `official_${keyword}_${term}`,
+          `${term}_fan_${keyword}`,
+          `${term}_club_${keyword}`,
+          `best_${keyword}_${term}`,
+          `only_${keyword}_${term}`,
+          `hello_${term}_${keyword}`,
+          `iam_${keyword}_${term}`,
+          `${keyword}_forever_${term}`,
+          `${keyword}_${term}_lover`,
+          `the_real_${keyword}_${term}`,
+          `${keyword}_${term}_${Math.floor(Math.random() * 10000)}`
+        );
       });
-      console.log(results, "result");
       setUsernames(results);
       setLoading(false);
     }, 1000);
@@ -244,7 +277,9 @@ const page = () => {
   const handleCopyBio = (event: React.MouseEvent<HTMLSpanElement>) => {
     event.stopPropagation();
     navigator.clipboard.writeText(username);
-    alert("username copied to clipboard!");
+    toast({
+      title: "Copied to clipboard!"
+    })
   };
 
   return (
@@ -256,11 +291,11 @@ const page = () => {
         Create a professional, eye-catching Instagram username in seconds
       </p>
       <div className="flex flex-col md:flex-row max-w-6xl mx-auto mt-10 gap-10 justify-center px-2">
-        <div className="w-full max-w-md space-y-4">
-          <div>
+        <div className="w-full max-w-md">
+          <div className="mt-4">
             <label
               htmlFor="category"
-              className="block text-sm font-medium text-gray-700"
+              className="block text-xl font-medium text-gray-700"
             >
               Select Category:
             </label>
@@ -281,10 +316,10 @@ const page = () => {
             </select>
           </div>
 
-          <div>
+          <div className="mt-4">
             <label
               htmlFor="keyword"
-              className="block text-sm font-medium text-gray-700"
+              className="block text-xl font-medium text-gray-700"
             >
               Enter name:
             </label>
@@ -300,39 +335,80 @@ const page = () => {
 
           <button
             onClick={generateUsernames}
-            className="w-full py-2 bg-indigo-600 text-white font-semibold rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none"
+            className="w-full py-2 bg-indigo-600 text-white font-semibold rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none mt-6"
           >
             Generate Usernames
           </button>
           {loading ? (
             <Loader />
-          ) : (
-            usernames.length > 0 && (
-              <div className="mt-6">
-                <h3 className="text-xl font-medium text-gray-800">
-                  Suggested Usernames:
-                </h3>
-                <div className="mt-4 h-[300px] w-full max-w-md border rounded-lg bg-white overflow-y-auto">
-                  <ul className="space-y-2 p-2">
-                    {usernames.map((name, index) => (
-                      <li
-                        key={index}
-                        className="p-2 border rounded-lg bg-gray-50 cursor-pointer hover:bg-indigo-600 hover:text-white flex justify-between items-center"
-                        onClick={() => handleUsernameClick(name)}
-                      >
-                        <span>{name}</span>
-                        <span
-                          className="bg-blue-500 text-white px-3 py-1 rounded-md cursor-pointer hover:bg-blue-600"
-                          onClick={handleCopyBio}
-                        >
-                          Copy
-                        </span>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
+          ) : usernames.length > 0 ? (
+            <div className="mt-6">
+              <h3 className="text-xl font-medium text-gray-800">
+                Suggested Usernames:
+              </h3>
+              <div className="mt-4 h-[300px] w-full max-w-md border rounded-lg bg-white overflow-y-auto">
+                <ul className="space-y-2 p-2">
+                  {usernames.map((name, index) => (
+                    <li
+                      key={index}
+                      className="p-2 border rounded-lg bg-gray-50 cursor-pointer hover:bg-indigo-600 hover:text-white flex justify-between items-center"
+                    >
+                      <span>{name}</span>
+                      <p className="flex justify-center items-center">
+                        <TooltipProvider>
+                          <Tooltip>
+                            <TooltipTrigger>
+                              <Image
+                                alt="preview"
+                                src={preview}
+                                width={28}
+                                height={28}
+                                onClick={() => handleUsernameClick(name)}
+                                className="mr-4"
+                              />
+                            </TooltipTrigger>
+                            <TooltipContent>
+                              <p>Preview Bio</p>
+                            </TooltipContent>
+                          </Tooltip>
+                        </TooltipProvider>
+                        <TooltipProvider>
+                          <Tooltip>
+                            <TooltipTrigger>
+                              <Image
+                                src={copy}
+                                alt="copy"
+                                width={28}
+                                height={28}
+                                onClick={handleCopyBio}
+                              />
+                            </TooltipTrigger>
+                            <TooltipContent>
+                              <p>Copy Bio</p>
+                            </TooltipContent>
+                          </Tooltip>
+                        </TooltipProvider>
+                      </p>
+                    </li>
+                  ))}
+                </ul>
               </div>
-            )
+            </div>
+          ) : (
+            <Card className="mt-12">
+              <div className="text-center py-12 text-gray-500 flex flex-col justify-center items-center">
+                <Image
+                  src={checklist}
+                  alt="checklist"
+                  width={100}
+                  height={100}
+                />
+                <p className="text-xl font-medium mt-4">No bio generated yet</p>
+                <p className="mt-2 text-sm">
+                  Select options and click generate to see your bio
+                </p>
+              </div>
+            </Card>
           )}
         </div>
         <div className="w-full flex justify-center">
